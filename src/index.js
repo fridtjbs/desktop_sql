0ff2f6d77c26e4edf536992d43122ee1c032da8d
fridtjbs <71632344+fridtjbs@users.noreply.github.com>
2020-10-28 11:26:38 +0100

    asdf

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
-import { pool } from './mysql-pool';
+import mysql from 'mysql';
 
-let studentList = document.getElementById('studentList');
+let knappInput = document.getElementById("knappInput");
 
-// Perform select-query that fetches all the Students table rows from the database
+// Setup MySQL-server connection pool
+let pool = mysql.createPool({
+  host: 'mysql-ait.stud.idi.ntnu.no',
+  user: 'fridtjbs', // Replace "username" with your username
+  password: 'ggfJYxW6', // Replace "password" with your password
+  database: 'fridtjbs', // Replace "username" with your username
+  connectionLimit: 1, // Reduce load on NTNU MySQL server
+});
+oppdater();
+knappInput.onclick = () => {
+  
+let navn = document.getElementById("inputN").value;
+let mail = document.getElementById("inputE").value;
+  
+  // Perform insert
+  pool.query(
+    
+    'INSERT INTO Students (name, email) VALUES (?, ?)',[navn, mail],
+    (error, results) => {
+      if (error) return console.error(error); // If error, show error in console (in red text) and return
+
+      console.log('Inserted id: ' + results.insertId); // Print out the id of the inserted row
+      oppdater();
+    }
+  );
+  }
+
+// Perform select-query that fetches all the Students table rows from the database()
+function oppdater(){
+while(studentList.firstChild){
+  studentList.removeChild(studentList.firstChild)
+}
 pool.query('SELECT * FROM Students', (error, results) => {
   if (error) return console.error(error); // If error, show error in console (in red text) and return
 
   for (let student of results) {
+    console.log(student.name + ', ' + student.email);
     let li = document.createElement('li');
+    li.innerHTML = "";
     li.innerText = student.name + ', ' + student.email;
     studentList.appendChild(li);
   }
 });
+}
--- src/mysql-pool.js
+++ src/mysql-pool.js
@@ -1,10 +1,19 @@
 import mysql from 'mysql';
 
 // Setup MySQL-server connection pool
-export let pool = mysql.createPool({
+let pool = mysql.createPool({
   host: 'mysql-ait.stud.idi.ntnu.no',
-  user: 'username', // Replace "username" with your username
-  password: 'password', // Replace "password" with your password
-  database: 'username', // Replace "username" with your username
+  user: 'fridtjbs', // Replace "username" with your username
+  password: 'ggfJYxW6', // Replace "password" with your password
+  database: 'fridtjbs', // Replace "username" with your username
   connectionLimit: 1, // Reduce load on NTNU MySQL server
+});
+
+// Perform select-query that fetches all the Students table rows from the database
+pool.query('SELECT * FROM Students', (error, results) => {
+  if (error) return console.error(error); // If error, show error in console (in red text) and return
+
+  for (let student of results) {
+    console.log(student.name, student.email);
+  }
 });
